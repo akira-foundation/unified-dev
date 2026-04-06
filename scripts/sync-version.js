f06469a85ef4fd20549009aa6cd8f7e7014b6de1
kid <kidiatoliny@gmail.com>
2026-04-06 21:22:34 +0200

    feat(release): add build and release workflows with version syncing

---
+++ scripts/sync-version.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+
+const packageJsonPath = path.join(__dirname, '../package.json')
+const tauriConfPath = path.join(__dirname, '../src-tauri/tauri.conf.json')
+const cargoTomlPath = path.join(__dirname, '../src-tauri/Cargo.toml')
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
+const tauriConf = JSON.parse(fs.readFileSync(tauriConfPath, 'utf8'))
+let cargoToml = fs.readFileSync(cargoTomlPath, 'utf8')
+
+const version = packageJson.version
+
+if (tauriConf.version !== version) {
+  tauriConf.version = version
+  fs.writeFileSync(tauriConfPath, JSON.stringify(tauriConf, null, 2) + '\n')
+}
+
+const cargoVersionRegex = /^version = "(.+?)"$/m
+const cargoMatch = cargoToml.match(cargoVersionRegex)
+const cargoVersion = cargoMatch ? cargoMatch[1] : null
+
+if (cargoVersion !== version) {
+  cargoToml = cargoToml.replace(cargoVersionRegex, `version = "${version}"`)
+  fs.writeFileSync(cargoTomlPath, cargoToml)
+}
+
+if (tauriConf.version !== version || cargoVersion !== version) {
+  console.log(`Synced version to ${version}`)
+} else {
+  console.log(`Version already synced (${version})`)
+}
